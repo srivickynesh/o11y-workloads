d0fb1e0ef253adb01eb07e32f0e49b45c78511d6
Sri Vignesh <sselvan@redhat.com>
2023-03-03 14:28:24 +0530

    Add scripts for log generation and fixes
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>

--- scripts/log_generation.sh
+++ scripts/log_generation.sh
@@ -1,10 +1,35 @@
-#!/usr/bin/env bash
-#!/usr/bin/env bash
-echo "Starting LOG Generation!"
-head -c 1G < /dev/urandom > $TESTS_PATH/random-data.log || exit 1
-ls -lh $TESTS_PATH/random-data.log
-if [ ! -f $TESTS_PATH/random-data.log ]; then
-    echo "Log Generation Failed"
-else
-    echo "Log Generation Successful"
+#!/usr/bin/bash
+
+oc create -f ../pipelines/pipeline-simple.yaml
+
+sleep 10
+
+oc project test
+
+oc logs test-pipeline-simple-log-generation-pod -c step-generate-random-echo > test.log
+if [ ! -f test.log ]; then
+    echo "File Not Found"
+    exit 1
+fi
+
+# Print log size
+
+echo "Log Size is : `ls -l test.log | awk '{print $5}'`"
+if [ $? -ne 0 ]; then
+    echo "Failure $?"
+    exit 1
+fi
+
+# Cleanup pipeline and log
+
+yes | tkn pr delete test-pipeline-simple
+if [ $? -ne 0 ]; then
+    echo "Failure $?"
+    exit 1
+fi
+
+rm -rf test.log
+if [  -f test.log ]; then
+    echo "File Found"
+    exit 1
 fi
